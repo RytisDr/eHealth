6b415b546dc4a6b5ee033ba3e3c80ae37f34467f
RytisDr <r.drazdauskas@gmail.com>
2019-06-14 23:36:35 +0200

    Styles header, changes sizes on scroll.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+const headerInitHeight = document.getElementById("index-header").clientHeight;
+
+window.addEventListener("scroll", () => {
+  const logo = document.getElementById("logo");
+  const slogan = document.getElementById("slogan");
+  const header = document.getElementById("index-header");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    logo.style.width = "70px";
+    logo.style.margin = "15px auto 0";
+    header.style.height = "80px";
+    slogan.style.opacity = "0";
+  } else {
+    logo.style.margin = "30px auto 0";
+    header.style.height = headerInitHeight;
+    logo.style.width = "130px";
+    slogan.style.opacity = "1";
+  }
+});
